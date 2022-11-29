3cced179919dc741778e0d5146256baa68c2601c
Rachel Kelly <racheldk126@gmail.com>
2022-11-29 10:10:09 -0800

    first commit

---
+++ src/views/AllQuestionsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="stuff">
+    <h1 class="title">All Questions Page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .stuff {
+    border: 5px blue solid;
+  }
+
+  .title {
+    border: 5px red solid;
+  }
+}
+</style>
